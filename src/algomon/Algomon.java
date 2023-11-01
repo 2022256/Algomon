d16ccda55b51efb60c512a3c7126df60ec337b66
2022256 <2022256@student.cct.ie>
2023-11-01 12:19:21 +0000

    Initial commit

---
+++ src/algomon/Algomon.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package algomon;
+
+/**
+ *
+ * @author Lecturer
+ * GitHub link:
+ * Group Member 1 Name:
+ * Group Member 1 Student Number:
+ * Group Member 2 Name:
+ * Group Member 2 Student Number:
+ */
+public class Algomon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
