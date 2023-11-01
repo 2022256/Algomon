3315607c40c0c37af12b60341cb3cfb513a44dd6
IvanPopoff <2022180@student.cct.ie>
2023-11-01 13:24:11 +0000

    added start game method
    created for Algomons to players

--- src/algomon/AlgomonGame.java
+++ src/algomon/AlgomonGame.java
@@ -9,12 +9,12 @@ import java.util.List;
 
 /**
  *
- * @author LecturerA
- * GitHub link:
- * Group Member 1 Name:
- * Group Member 1 Student Number:
- * Group Member 2 Name:
- * Group Member 2 Student Number:
+ * @author Lecturer
+ * GitHub link: https://github.com/2022256/Algomon
+ * Group Member 1 Name: Andrei Chistiakov
+ * Group Member 1 Student Number: 2022256
+ * Group Member 2 Name: Ivan Popov
+ * Group Member 2 Student Number: 2022180
  */
 
 class Ability { // Abilities for Algomons
@@ -96,6 +96,60 @@ public class AlgomonGame { // renamed class
     }
     
     }
+    
+ public static void startGame(){
+     Algomon algomon1 = new Algomon("Fire", 100, 20, 10, 30); 
+        algomon1.offensiveAbility = new Ability("Offensive", "Fire Attack"); 
+        algomon1.defensiveAbility = new Ability("Defensive", "Fire Defense"); 
+        algomon1.healingAbility = new Ability("Healing", "Fire Healing"); 
+ 
+        Algomon algomon2 = new Algomon("Water", 120, 15, 12, 25); 
+        algomon2.offensiveAbility = new Ability("Offensive", "Water Attack"); 
+        algomon2.defensiveAbility = new Ability("Defensive", "Water Defense"); 
+        algomon2.healingAbility = new Ability("Healing", "Water Healing"); 
+ 
+        Algomon algomon3 = new Algomon("Earth", 110, 18, 15, 28); 
+        algomon3.offensiveAbility = new Ability("Offensive", "Earth Attack"); 
+        algomon3.defensiveAbility = new Ability("Defensive", "Earth Defense"); 
+        algomon3.healingAbility = new Ability("Healing", "Earth Healing"); 
+ 
+        Algomon algomon4 = new Algomon("Sun", 130, 25, 8, 35); 
+        algomon4.offensiveAbility = new Ability("Offensive", "Sun Attack"); 
+        algomon4.defensiveAbility = new Ability("Defensive", "Sun Defense"); 
+        algomon4.healingAbility = new Ability("Healing", "Sun Healing"); 
+ 
+        Player player1 = new Player(); 
+        player1.addAlgomon(algomon1); 
+        player1.addAlgomon(algomon2); 
+ 
+        Player player2 = new Player(); 
+        player2.addAlgomon(algomon3); 
+        player2.addAlgomon(algomon4); 
+ 
+        System.out.println("Algomon game prototype initialized!"); 
+        System.out.println("Player 1 Algomon:"); 
+        for (Algomon algomon : player1.algomons) { 
+            System.out.println("Type: " + algomon.type); 
+            System.out.println("Health: " + algomon.health); 
+            algomon.offensiveAbility.activate(); 
+            algomon.defensiveAbility.activate(); 
+            algomon.healingAbility.activate(); 
+            System.out.println("----------------------"); 
+        } 
+ 
+        System.out.println("Player 2 Algomon:"); 
+        for (Algomon algomon : player2.algomons) { 
+            System.out.println("Type: " + algomon.type); 
+            System.out.println("Health: " + algomon.health); 
+            algomon.offensiveAbility.activate(); 
+            algomon.defensiveAbility.activate(); 
+            algomon.healingAbility.activate(); 
+            System.out.println("----------------------"); 
+        } 
+    } 
 }
+ 
+     
+    
 
 
