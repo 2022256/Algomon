7db32e30a1382bfb109cc7fa24ed466779cbffdd
2022256 <2022256@student.cct.ie>
2023-11-01 12:36:29 +0000

    Created Ability class for Algomons

--- src/algomon/Algomon.java
+++ src/algomon/Algomon.java
@@ -6,20 +6,26 @@ package algomon;
 
 /**
  *
- * @author Lecturer
+ * @author LecturerA
  * GitHub link:
  * Group Member 1 Name:
  * Group Member 1 Student Number:
  * Group Member 2 Name:
  * Group Member 2 Student Number:
  */
+
+class Ability { // Abilities for Algomons
+    String type;
+    String name;
+}
+
 public class Algomon {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
